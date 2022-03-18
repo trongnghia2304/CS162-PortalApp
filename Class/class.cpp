143c5a406cf60d81ce79e1d4005b0154d78daa25
Thang2103 <100982600+Thang2103@users.noreply.github.com>
2022-03-18 12:39:50 +0700

    Update class.cpp

--- Class/class.cpp
+++ Class/class.cpp
@@ -3,3 +3,10 @@
 void checkMyClass(Class a){
     printStudentNode(a.student_list);
 }
+Class createClass(string C_id, string H_Tea, StudentNode* StuLis) {
+	Class new_Class;
+	new_Class.class_id.assign(C_id);
+	new_Class.head_teacher.assign(H_Tea);
+	new_Class.student_list = StuLis;
+	return new_Class;
+}
