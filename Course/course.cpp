1778eef7f57a05debf08d91994cc1ce086028f36
trongnghia2304 <nghiatrongnguyen2304@gmail.com>
2022-03-18 16:11:28 +0700

    timetable

--- Course/course.cpp
+++ Course/course.cpp
@@ -68,3 +68,43 @@ void unsubscribeCourse(Student &me, CourseNode &a){
     // delete the student from the course list
     removeStudentNode(&a.course.student_list, me.student_id);
 }
+void timeTable(Student me){
+
+    cout << "id, course, teacher, credit, timetable \n";
+    Student you = me;
+    while (you.my_course->course->next != nullptr) you.my_course = you.my_course->next;
+    int sem = you.my_course->course->semester_id;
+    int year = you.my_course->course->year;
+
+    while (me.my_course->course->year != year && me.my_course->course->semester_id != sem) me.my_course = me.my_course->next;
+    for (MyCourse* cur = me.my_course; cur && cur->course->semester_id == sem; cur = cur->next){
+        cout << cur->course->course.course_id << "  " << cur->course->course.course_name << "  " << cur->course->course.teacher_name << "  " << cur->course->course.num_credit << "  ";
+        for (int i = 0; i < 2; i++){
+            switch (cur->course->course.teaching_session[i].day_of_the_week){
+                case 1:
+                    cout << "Sun";
+                    break;
+                case 2:
+                    cout << "Mon";
+                    break;
+                case 3:
+                    cout << "Tue";
+                    break;
+                case 4:
+                    cout << "Wed";
+                    break;
+                case 5:
+                    cout << "Thu";
+                    break;
+                case 6:
+                    cout << "Fri";
+                    break;
+                default:
+                    cout << "Sat";
+                    break;
+            }
+            cout << "-" << cur->course->course.teaching_session[i].session_no << "  ";
+        }
+        cout << endl;
+    }
+}
