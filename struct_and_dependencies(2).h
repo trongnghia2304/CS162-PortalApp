c4852c3bfe439f2fd178d8e30f6751dfcc2bbc6d
jolliebrown <michealbradcute@gmail.com>
2022-03-23 22:18:25 +0700

    change struct_and_dependencies.h

--- struct_and_dependencies(2).h
+++ struct_and_dependencies(2).h
@@ -1,102 +1,37 @@
 #pragma once
-#include "struct_and_dependencies.h"
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <string>
+#include <math.h>
+#include <sstream>
 
-struct Student
-{
-    int num;
-    string student_id;
-    string first_name;
-    string last_name;
-    bool gender;
-    string dob;
-    string social_id;
-    string password;
-    string student_class;
-    MyCourse *my_course;
-    string display_name;
-};
 
-struct StudentNode
-{
-    Student student;
-    StudentNode *next;
-};
 
-struct Class
-{
-    string class_id;
-    string head_teacher;
-    StudentNode *student_list;
-};
+using namespace std;
 
-struct ClassNode
-{
-    Class my_class;
-    ClassNode* next;
-};
+struct Student;
 
-struct Session
-{
-    int day_of_the_week; //1-sun, 2-mon
-    int session_no;
-};
+struct StudentNode;
 
-struct Score
-{
-    int process;
-    int mid;
-    int fin;
-};
+struct Class;
 
-struct Course
-{
-    string course_id;
-    string course_name;
-    string teacher_name;
-    int num_credit;
-    int max_students = 50;
-    Session teaching_session[2];
-    Session exam_session[2];
-    StudentNode *student_list;
-    Score score;
-};
+struct ClassNode;
 
-struct CourseNode
-{
-    int semester_id;
-    int year;
-    Course course;
-    CourseNode *next;
-};
+struct Session;
 
-struct MyCourse
-{
-    CourseNode *course;
-    MyCourse *next;
-    MyCourse(CourseNode &a) : course(&a), next(nullptr) {};
-};
+struct Score;
 
-struct Semester
-{
-    string semester_id;
-    CourseNode *course_list;
-};
+struct Course;
 
-struct SemesterNode
-{
-    Semester sem;
-    SemesterNode* next;
-};
+struct CourseNode;
 
-struct Year
-{
-    string year_id;
-    ClassNode* list_class;
-    SemesterNode *list_sem;
-};
+struct MyCourse;
 
-struct YearNode
-{
-    Year school_year;
-    YearNode* next;
-};
+struct Semester;
+
+struct SemesterNode;
+
+struct Year;
+
+struct YearNode;
--- struct_and_dependencies.h
+++ struct_and_dependencies.h
@@ -1,36 +1,102 @@
 #pragma once
-#include <iostream>
-#include <fstream>
-#include <string.h>
-#include <string>
-#include <math.h>
-#include <sstream>
+#include "struct_and_dependencies(2).h"
 
-using namespace std;
+struct Student
+{
+    int num;
+    string student_id;
+    string first_name;
+    string last_name;
+    bool gender;
+    string dob;
+    string social_id;
+    string password;
+    string student_class;
+    MyCourse *my_course;
+    string display_name;
+};
 
-struct Student;
+struct StudentNode
+{
+    Student student;
+    StudentNode *next;
+};
 
-struct StudentNode;
+struct Class
+{
+    string class_id;
+    string head_teacher;
+    StudentNode *student_list;
+};
 
-struct Class;
+struct ClassNode
+{
+    Class my_class;
+    ClassNode* next;
+};
 
-struct ClassNode;
+struct Session
+{
+    int day_of_the_week; //1-sun, 2-mon
+    int session_no;
+};
 
-struct Session;
+struct Score
+{
+    int process;
+    int mid;
+    int fin;
+};
 
-struct Score;
+struct Course
+{
+    string course_id;
+    string course_name;
+    string teacher_name;
+    int num_credit;
+    int max_students = 50;
+    Session teaching_session[2];
+    Session exam_session[2];
+    StudentNode *student_list;
+    Score score;
+};
 
-struct Course;
+struct CourseNode
+{
+    int semester_id;
+    int year;
+    Course course;
+    CourseNode *next;
+};
 
-struct CourseNode;
+struct MyCourse
+{
+    CourseNode *course;
+    MyCourse *next;
+    MyCourse(CourseNode &a) : course(&a), next(nullptr) {};
+};
 
-struct MyCourse;
+struct Semester
+{
+    string semester_id;
+    CourseNode *course_list;
+};
 
-struct Semester;
+struct SemesterNode
+{
+    Semester sem;
+    SemesterNode* next;
+};
 
-struct SemesterNode;
+struct Year
+{
+    string year_id;
+    ClassNode* list_class;
+    SemesterNode *list_sem;
+};
 
-struct Year;
-
-struct YearNode;
-
+struct YearNode
+{
+    Year school_year;
+    YearNode* next;
+};
