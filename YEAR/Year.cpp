2e572a0f315949789298da7606de21ebcb69e63d
Thang2103 <100982600+Thang2103@users.noreply.github.com>
2022-03-23 20:43:55 +0700

    Update Year.cpp

--- YEAR/Year.cpp
+++ YEAR/Year.cpp
@@ -1,1 +1,8 @@
-
+Year createYear(ClassNode* ClassList,SemesterNode *SemList) {
+	Year new_year;
+	cout << "Please enter the name of Year : ";
+	getline(cin, new_year.year_id);
+	new_year.list_class = ClassList;
+	new_year.list_sem = SemList;
+	return new_year;
+}
