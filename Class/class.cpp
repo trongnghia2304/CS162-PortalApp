ec640b7b0fcd7b407ad9e6079440046ff2d87df2
Thang2103 <100982600+Thang2103@users.noreply.github.com>
2022-03-24 08:13:14 +0700

    Update class.cpp

--- Class/class.cpp
+++ Class/class.cpp
@@ -3,12 +3,20 @@
 void checkMyClass(Class a){
     printStudentNode(a.student_list);
 }
-Class createClass(string C_id, string H_Tea ) {
-	Class new_Class;
-	new_Class.class_id.assign(C_id);
-	new_Class.head_teacher.assign(H_Tea);
-	
-	return new_Class;
+ClassNode* Create_List_Class(ClassNode* &Class_head,string class_name,string head_teacher) {
+	ClassNode* pClass = new ClassNode;
+	pClass->my_class.class_id = class_name;
+	pClass->my_class.head_teacher = head_teacher;
+	pClass->next = NULL;
+	ClassNode* tmp = Class_head;
+	if (tmp == NULL)
+		Class_head = pClass;
+	else {
+		while (tmp->next != NULL) {
+			tmp = tmp->next;
+		}tmp->next = pClass;
+	}
+	return Class_head;
 }
 void AddStudenttoClass(Class &a, StudentNode* stu) {
 	a.student_list = NULL;
