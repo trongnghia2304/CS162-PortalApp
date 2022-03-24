b581ce33d69b59c6a36bb4e4960903c5e925bf0c
Thang2103 <100982600+Thang2103@users.noreply.github.com>
2022-03-24 16:25:46 +0700

    Update Year.cpp

--- YEAR/Year.cpp
+++ YEAR/Year.cpp
@@ -1,10 +1,10 @@
 #include "Year.h"
-YearNode*createYear(YearNode*&Year_head,ClassNode *Class_head,SemesterNode *Semester_head) {
+YearNode*createYear(YearNode*&Year_head) {
 	YearNode* pYear = new YearNode;
 	cout << " Please enter name of the year (XXXX-YYYY) :";
 	getline(cin, pYear->school_year.year_id);
-	pYear->school_year.list_class = Class_head;
-	pYear->school_year.list_sem = Semester_head;
+	pYear->school_year.list_class =NULL;
+	pYear->school_year.list_sem = NULL;
 	pYear->next = NULL;
 	YearNode* p = Year_head;
 	if (p != NULL)
