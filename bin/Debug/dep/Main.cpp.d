e628bcb089387d448d0a26ac19dc02e58c072308
jolliebrown <michealbradcute@gmail.com>
2022-03-28 10:03:08 +0700

    Add SFML

---
+++ bin/Debug/dep/Main.cpp.d
@@ -0,0 +1,8 @@
+bin/Debug/obj/Main.cpp.o: src/Main.cpp src/Platform/Platform.hpp \
+ src/Platform/Win32/WindowsPlatform.hpp src/Platform/IPlatform.hpp
+
+src/Platform/Platform.hpp:
+
+src/Platform/Win32/WindowsPlatform.hpp:
+
+src/Platform/IPlatform.hpp:
