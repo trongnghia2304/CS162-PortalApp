d4298c3aa6f6af457206310fc36f1ac6961ea82a
Thang2103 <100982600+Thang2103@users.noreply.github.com>
2022-03-31 19:04:14 +0700

    Update class_classList.cpp

--- class_classList.cpp
+++ class_classList.cpp
@@ -30,28 +30,28 @@ ClassNode *searchClassNode(ClassNode *p_head, string p_class_id)
     }
     return temp;
 }
-void pushNewClassNode(ClassNode **p_head, Class p_new_class)
+void pushNewClassNode(ClassNode *&p_head, Class p_new_class)
 {
     ClassNode *new_class_node = initClassNode(p_new_class);
-    if (!(*p_head))
+    if (p_head==NULL)
     {
-        *p_head = new_class_node;
+        p_head = new_class_node;
         return;
     }
-    new_class_node->next = *p_head;
-    *p_head = new_class_node;
+    new_class_node->next = p_head;
+    p_head = new_class_node;
 }
-void appendNewClassNode(ClassNode **p_head, Class p_new_class)
+void appendNewClassNode(ClassNode *p_head, Class p_new_class)
 {
     ClassNode *new_class_node = initClassNode(p_new_class);
-    if (!(*p_head))
+    if (p_head==NULL)
     {
-        *p_head = new_class_node;
+        p_head = new_class_node;
         return;
     }
     else
     {
-        ClassNode *temp = *p_head;
+        ClassNode *temp = p_head;
         while (temp->next)
         {
             temp = temp->next;
@@ -84,5 +84,18 @@ void removeClassNode(ClassNode **p_head, string p_class_id)
     }
     temp->next = founded_class->next;
     delete founded_class;
+}
+void addStudenttoClass(ClassNode*&p_head_class, StudentNode*p_head_student) {
+	ClassNode *tmp_class = p_head_class;
+	StudentNode* tmp_student = p_head_student;
+	while (tmp_class!=NULL&&tmp_class->next!= NULL) {
+		while (tmp_student != NULL && tmp_student->next != NULL) {
+			if (tmp_student->student.student_class == tmp_class->my_class.class_id) {
+				appendNewStudentNode(tmp_class->my_class.student_list, tmp_student->student);
+			}
+			tmp_student = tmp_student->next;
+		}tmp_class = tmp_class->next;
+		tmp_student = p_head_student;
+	}
 }
 //--------------------------------------------------------------------------------------------------
